6e3b39015ac1796f62f38f4dc9e5a7a56ad15c70
Jimmy Tran <jimmy03tran@gmail.com>
2026-06-26 21:51:33 +0100

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        stringS = {}
+        stringT = {}
+
+        for char in s:
+            if char in stringS:
+                stringS[char] += 1
+            else:
+                stringS[char] = 0
+
+        for char1 in t:
+            if char1 in stringT:
+                stringT[char1] += 1
+            else:
+                stringT[char1] = 0
+
+        if stringS == stringT:
+            return True
+        return False
